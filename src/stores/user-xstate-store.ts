a0bec4b2d45e29cc89bf1a240fc89357e5104cdd
itswil <ww8668@gmail.com>
2025-01-03 15:37:01 +0800

    Remove unused selectors

--- src/stores/user-xstate-store.ts
+++ src/stores/user-xstate-store.ts
@@ -30,11 +30,4 @@ export const store = createStore({
 	},
 });
 
-export const useUserAgeX = () =>
-	useSelector(store, (state) => state.context.age);
-export const useUserNameX = () =>
-	useSelector(store, (state) => state.context.name);
-export const useUserSkillsX = () =>
-	useSelector(store, (state) => state.context.skills);
-
 export const useUserX = () => useSelector(store, (state) => state.context);
